e2a774395352fa15957af28251ed01272bb954f1
gunho30811 <100767288+gunho30811@users.noreply.github.com>
2022-05-30 20:39:43 +0900

    Revert "?"
    This reverts commit 0416851418201a4c590ad656fcbb4a77ed05daa8.

--- login.php
+++ login.php
@@ -41,6 +41,27 @@
         <a href="forgot.php">Forgot Password?</a>
     </div>
     </section>
+    <script> 
+ let id = document.querySelector('#id')
+        let pw = document.querySelector('#pw')
+        let btn = document.querySelector('#btn')
 
+        btn.addEventListener('click', () => {
+            if(id.value == "") {
+                label = id.nextElementSibling
+                label.classList.add('warning')
+                setTimeout(() => {
+                    label.classList.remove('warning')
+                }, 1500)
+            } else if(pw.value == "") {
+                label = pw.nextElementSibling
+                label.classList.add('warning')
+                setTimeout(() => {
+                    label.classList.remove('warning')
+                }, 1500)
+            }
+        })
+        
+    </script>
 </body>
 </html>
