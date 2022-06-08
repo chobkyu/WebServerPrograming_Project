ba62ad9054b4800c4e2b869c830c5ee7120e4922
youngw77 <103637293+youngw77@users.noreply.github.com>
2022-06-08 18:50:26 +0900

    2022-06-08 18:50

--- loginSite.php
+++ loginSite.php
@@ -3,9 +3,11 @@
     $pw=$_POST["pw"];
 
     $con = mysqli_connect("database-1.c9g35ixldt8h.ap-northeast-2.rds.amazonaws.com", "admin", "00000000", "project");//공백란에 디비 비번 입력 
-    $sql = "select * from member where id='$id'"; 
+    $sql = "select * from member where UserId='$id'"; 
     $result = mysqli_query($con, $sql); 
     $num_match = mysqli_num_rows($result); 
+
+
     if (!$num_match) 
     {
         echo(" 
--- member_insert.php
+++ member_insert.php
@@ -5,8 +5,8 @@
     $email = $_POST["email"];
     $answer = $_POST["answer"];
 
-    $con = mysqli_connect("localhost", "root", "", "login");//공백란에 디비 비번 입력 
-    $sql = "insert into mem(id, pass, name, email, answer) "; 
+    $con = mysqli_connect("database-1.c9g35ixldt8h.ap-northeast-2.rds.amazonaws.com", "admin", "00000000", "project");//공백란에 디비 비번 입력 
+    $sql = "insert into member(UserId, UserPw, UserNickName, UserEmail, UserQuestion) "; 
     $sql .= "values('$id', '$password', '$name', '$email', '$answer')"; 
     mysqli_query($con, $sql); // $sql에 저장된 명령 실행 ⑥
 
