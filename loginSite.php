9da973505a0ebd855af5151df80fdc0594b833a2
gunho30811 <100767288+gunho30811@users.noreply.github.com>
2022-06-16 19:58:36 +0900

    2022-06-16
    비밀번호 틀릴 시에 로그인 안되게.

--- loginSite.php
+++ loginSite.php
@@ -3,7 +3,7 @@
     $pw=$_POST["pw"];
 
     $con = mysqli_connect("database-1.c9g35ixldt8h.ap-northeast-2.rds.amazonaws.com", "admin", "00000000", "project");//공백란에 디비 비번 입력 
-    $sql = "select * from member where UserId='$id'"; 
+    $sql = "select * from member where UserId='$id'and Userpw='$pw'"; 
     $result = mysqli_query($con, $sql); 
     $num_match = mysqli_num_rows($result); 
 
@@ -12,7 +12,7 @@
     {
         echo(" 
             <script> 
-                window.alert('등록되지 않은 아이디입니다!') 
+                window.alert('아이디 혹은 비밀번호가 일치하지 않습니다.') 
                 history.go(-1) 
             </script>
         "); 
@@ -22,7 +22,7 @@
     else{
         session_start( );
         $_SESSION["userId"] = $id;
-
+        
         echo("
             
             <script>
